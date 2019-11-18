e96ba373780f3429e41eebb22b3d09d2484e48e3
kappa0923 <tom.kappa0923@gmail.com>
2019-11-18 21:54:15 +0900

    Cloud Firestoreの構造修正

--- functions/index.js
+++ functions/index.js
@@ -7,14 +7,8 @@ const app = express();
 admin.initializeApp(functions.config().firebase);
 
 app.use(cors({ origin: '*' }))
-<<<<<<< HEAD
 app.get('/api/speaker/:id', (req, res) => {
-=======
-app.get('/speaker/:id', async (req, res) => {
->>>>>>> Admin SDKを利用するように修正
-    //FIXME これが不要になるようにFirestore側のデータをマイグレーションする
-    const base = req.params.id;
-    const speakerID = base.replace(/-/g, '.');
+    const speakerID = req.params.id;
     const db = admin.firestore();
 
     const responseData = await db.collection('speakers').doc(speakerID).get()
